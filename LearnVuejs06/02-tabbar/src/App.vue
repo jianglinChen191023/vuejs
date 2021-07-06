8f03fb1117bfce00b1d0507cbec7551ca72358e9
jianglinChen191023 <1912623646@qq.com>
2021-07-06 23:36:46 +0800

    TabBar 案例

---
+++ LearnVuejs06/02-tabbar/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <tab-bar>
+      <tab-bar-item>
+        <img slot="item-icon" src="./assets/img/home.svg" alt="">
+        <div slot="item-text">首页</div>
+      </tab-bar-item>
+      <tab-bar-item>
+        <img slot="item-icon" src="./assets/img/category.svg" alt="">
+        <div slot="item-text">分类</div>
+      </tab-bar-item>
+      <tab-bar-item>
+        <img slot="item-icon" src="./assets/img/shopcart.svg" alt="">
+        <div slot="item-text">购物车</div>
+      </tab-bar-item>
+      <tab-bar-item>
+        <img slot="item-icon" src="./assets/img/profile.svg" alt="">
+        <div slot="item-text">个人中心</div>
+      </tab-bar-item>
+    </tab-bar>
+  </div>
+</template>
+
+<script>
+import TabBar from './components/tabbar/TabBar'
+import TabBarItem from './components/tabbar/TabBarItem'
+
+export default {
+  name: 'App',
+  components: {
+    TabBar,
+    TabBarItem
+  }
+}
+</script>
+
+<style>
+@import "./assets/css/base.css";
+</style>
+
