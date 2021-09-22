1ad4efd594bd3b178f94b585f51e9498179a0059
jianglinChen191023 <1912623646@qq.com>
2021-09-22 23:47:26 +0800

    2. 购物车-购物车商品列表展示

--- LearnVuejs10/supermall/src/views/cart/Cart.vue
+++ LearnVuejs10/supermall/src/views/cart/Cart.vue
@@ -1,27 +1,35 @@
 <template>
   <div class="cart">
+    <!--  导航  -->
     <nav-bar class="nav-bar">
       <div slot="center">购物车({{ length }})</div>
     </nav-bar>
+
+    <!--  商品的列表  -->
+    <cart-list/>
+
+    <!--  底部汇总  -->
   </div>
 </template>
 
 <script>
 import NavBar from "components/common/navbar/NavBar";
 
+import CartList from "./childComps/CartList";
+
 import {mapGetters} from 'vuex'
 
 export default {
   name: "Cart",
   components: {
-    NavBar
+    NavBar,
+    CartList
   },
   computed: {
     // 两种语法
     // ...mapGetters(['cartLength', 'cartList'])
     ...mapGetters({
-      length: 'cartLength',
-      list: 'cartList'
+      length: 'cartLength'
     })
   }
 }
---
+++ LearnVuejs10/supermall/src/views/cart/childComps/CartList.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="cart-list">
+    <scroll class="content" ref="scroll">
+      <cart-list-item v-for="item in cartList" :key="item.id" :product="item"/>
+    </scroll>
+  </div>
+</template>
+
+<script>
+import {mapGetters} from 'vuex'
+
+import Scroll from "components/common/scroll/Scroll";
+
+import CartListItem from "./CartListItem";
+
+export default {
+  name: "CartList",
+  components: {
+    Scroll,
+    CartListItem
+  },
+  computed: {
+    ...mapGetters({
+      cartList: 'cartList'
+    })
+  },
+  activated() {
+    this.$refs.scroll.refresh();
+  }
+}
+</script>
+
+<style scoped>
+.content {
+  overflow: hidden;
+
+  width: 100%;
+  position: absolute;
+  top: 44px;
+  bottom: 49px;
+}
+</style>
