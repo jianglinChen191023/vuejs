00c170c38be5a5788d9263e10989b239c2dee970
jianglinChen191023 <1912623646@qq.com>
2021-09-23 19:48:25 +0800

    3. 购物车-购物车列表的 item 展示

---
+++ LearnVuejs10/supermall/src/components/content/checkButton/CheckButton.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="check-button" :class="{'check': isChecked}">
+    <img src="~assets/img/cart/tick.svg" alt="" v-show="isChecked">
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CheckButton",
+  props: {
+    isChecked: {
+      type: Boolean,
+      default() {
+        return false;
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.check-button {
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  border: 2px solid #aaa;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.check-button img {
+  width: 12px;
+}
+
+.check {
+  border-color: red;
+  background-color: red;
+}
+</style>
